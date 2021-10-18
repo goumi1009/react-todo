2592a3e9372747baee0af5b763372c1aa58aebb3
goumi <goumi1009@gmail.com>
2021-10-18 14:45:41 +0900

    feat: add @emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { css } from '@emotion/react'
+
+const style = css`
+  color: hotpink;
+`
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+    <h1 css={{fontSize: 40, color: 'blue'}}>제목 입니다.</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <SomeComponent></SomeComponent>
+    </div>
   );
 }
 
